9f39ac14c12bb9fbc65c2109780f6227fd681100
dlucic <Akodm44!>
2019-10-21 21:07:34 +0200

    Initial commit

---
+++ src/test/java/com/davorin/onboarding/OnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davorin.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
