933dbac42cb44031d346508f76ec398cbef35b50
dlucic <Akodm44!>
2019-10-27 22:46:57 +0100

    Add models Add services Add repositories Add controllers

--- src/main/java/com/davorin/onboarding/controller/FieldController.java
+++ src/main/java/com/davorin/onboarding/controller/FieldController.java
@@ -2,7 +2,6 @@ package com.davorin.onboarding.controller;
 
 import com.davorin.onboarding.model.Field;
 import com.davorin.onboarding.service.FieldService;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/field")
 public class FieldController {
-
-    private static final Logger logger = Logger.getLogger(FieldController.class);
 
     private FieldService fieldService;
 
